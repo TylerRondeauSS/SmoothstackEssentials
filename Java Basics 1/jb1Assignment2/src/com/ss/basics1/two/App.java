16cdd8afafdce27242a9d8a54ad663a25dce8a58
Tyler Rondeau <92643422+TylerRondeauSS@users.noreply.github.com>
2021-10-20 13:34:46 -0400

    Error Catching
    Added error catching to assignment 2 for inputs that are non-numerical.

--- Java Basics 1/jb1Assignment2/src/com/ss/basics1/two/App.java
+++ Java Basics 1/jb1Assignment2/src/com/ss/basics1/two/App.java
@@ -6,14 +6,14 @@ public class App {
         int guesses = 4;
         int answer = (int) (Math.random()*100 + 1);
         //print out the number for debugging
-        System.out.println(answer);
+        //System.out.println(answer);
         System.out.println("Hello, please guess a number between 1 and 100!");
-        int userAnswer = myScanner.nextInt();
+        int userAnswer = onlyNumbers(myScanner.nextLine(), myScanner);
         while(guesses!=0){
             //Check if guess is within parameters
-            if (userAnswer <=0 || userAnswer >100) {
+            if (userAnswer <1 || userAnswer >100) {
                 System.out.println("Guess is not between 1 and 100. Please guess again!");
-                userAnswer = myScanner.nextInt();
+                userAnswer = onlyNumbers(myScanner.nextLine(), myScanner);
             }
             //Check if guess is correct
             else if (userAnswer == answer ){
@@ -30,11 +30,24 @@ public class App {
             //Guess was incorrect, guess again
             else {
                 System.out.println("Your guess was incorrect, Please guess again! You have " + guesses + " guesses remaining.");
-                userAnswer = myScanner.nextInt();
+                userAnswer = onlyNumbers(myScanner.nextLine(), myScanner);
                 guesses--;
             }
         }
         myScanner.close();
-        System.out.println("You ran out of guesses! Sorry!");
+        System.out.println("You ran out of guesses! Sorry! The correct answer was : " + answer);
+    }
+
+    //Method that tries to parse an integer from the user input, and if it fails requests the user re-enter input with only numbers.
+    private static int onlyNumbers(String userAnswer, Scanner myScanner){
+        int willItInt = 0;
+        try{
+            willItInt = Integer.parseInt(userAnswer);
+        }
+        catch(NumberFormatException nfe) {
+            System.out.println("Guess is not a number. Please input only numbers between 1 and 100!");
+            return onlyNumbers(myScanner.nextLine(), myScanner);
+        }
+        return willItInt;
     }
 }
