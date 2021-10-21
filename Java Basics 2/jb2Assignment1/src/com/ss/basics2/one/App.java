213111de5dfe1667ec9ff87746a0274bf5f3c982
Tyler Rondeau <92643422+TylerRondeauSS@users.noreply.github.com>
2021-10-21 13:34:12 -0400

    Created JB2 Assignment 2
    Created Java Basics 2 Assignment 2 fully and edited launch.json file to input command line arguments.

--- Java Basics 2/jb2Assignment1/src/com/ss/basics2/one/App.java
+++ Java Basics 2/jb2Assignment1/src/com/ss/basics2/one/App.java
@@ -2,6 +2,21 @@ package com.ss.basics2.one;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] argsInts;
+        int addedTotal = 0;
+        argsInts = new int[args.length];
+        try{
+            for(int i =0; i<args.length;i++){
+                argsInts[i] = Integer.parseInt(args[i]);
+            }
+        }
+        catch (NumberFormatException nfe){
+            System.err.println("Must enter only integers");
+            System.exit(1);
+        }
+        for(int i = 0; i < argsInts.length;i++){
+            addedTotal += argsInts[i];
+        }
+        System.out.println("The total of all values passed is : " + addedTotal);
     }
 }
--- Java Basics 2/jb2Assignment3/src/com/ss/basics2/three/App.java
+++ Java Basics 2/jb2Assignment3/src/com/ss/basics2/three/App.java
@@ -1,5 +1,5 @@
 package com.ss.basics2.three;
-
+//Display class prints area not shape
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
