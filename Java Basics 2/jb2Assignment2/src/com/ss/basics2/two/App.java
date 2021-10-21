b74b2f3c7f47351319950cec3f08fe3948b44907
Tyler Rondeau <92643422+TylerRondeauSS@users.noreply.github.com>
2021-10-21 13:54:31 -0400

    Created jb2 Assignment 2
    Fully created Java Basics 2 Assignment 2 with the ability to use a premade Array or an array of random numbers between 1 and 100.

--- Java Basics 2/jb2Assignment2/src/com/ss/basics2/two/App.java
+++ Java Basics 2/jb2Assignment2/src/com/ss/basics2/two/App.java
@@ -2,6 +2,21 @@ package com.ss.basics2.two;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Basic filled array for debugging
+        int[][] twoDArray = {{1,2,3}, {4,5,6}, {7,8,9}};
+        /*code to make array random
+        for(int row = 0; row < twoDArray.length;row++){
+            for(int col = 0; col < twoDArray[row].length;col++){
+                twoDArray[row][col] = (int) (Math.random()*100 + 1);
+            }
+        }*/
+        int maxValue = twoDArray[0][0], maxRow=0, maxCol = 0;
+        //traverse array from top left to bottom right by row then column
+        for(int row = 0; row < twoDArray.length;row++){
+            for(int col = 0; col < twoDArray[row].length;col++){
+                if(twoDArray[row][col] > maxValue) maxValue = twoDArray[row][col]; maxRow = row; maxCol = col;
+            }
+        }
+        System.out.println("The highest value in the 2D Array is : " + maxValue + " at position : [" + maxRow + "][" + maxCol + "]");
     }
 }
