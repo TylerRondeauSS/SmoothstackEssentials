12ea9a58c0ccaf98f3c33275e58bceefec1eadf0
Tyler Rondeau <92643422+TylerRondeauSS@users.noreply.github.com>
2021-10-24 21:34:48 -0400

    Finished jb3Assignment2 and jb3Assignment3
    Created and completed jb3Assignment2 and jb3Assignment3 as well as edits to the text files used for both assignments.

--- Java Basics 3/jb3Assignment2/src/com/ss/basics3/two/App.java
+++ Java Basics 3/jb3Assignment2/src/com/ss/basics3/two/App.java
@@ -1,15 +1,24 @@
 package com.ss.basics3.two;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.nio.file.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        App runTest = new App();
         //path to our test file
-        String testPath = "D:\\Downloads\\Personal\\Smoothstack\\SmoothstackEssentials\\Java Basics 3\\testFiles\\jb3Assignment2TestDoc";
-        File appendTest = new File(testPath);
-        //Make sure the file exists and is a file not a directory and we can write to it
-        if(appendTest.exists() && appendTest.isFile() && appendTest.canWrite()){
-            
+        final String testPath = "D:\\Downloads\\Personal\\Smoothstack\\SmoothstackEssentials\\Java Basics 3\\testFiles\\jb3Assignment2TestDoc.txt";
+        final String testText = "\nI added text to the file again!";
+        runTest.writeFile(testPath, testText);
+    }
+    public void writeFile(String path, String text){
+        try(BufferedReader read = new BufferedReader(new FileReader(path))){
+            Files.write(Paths.get(path), text.getBytes(), StandardOpenOption.APPEND);
+        }
+        catch(IOException e){
+            System.err.println("File not found, please check that the file exists and the path is correct");
         }
     }
 }
--- Java Basics 3/jb3Assignment3/src/com/ss/basics3/three/App.java
+++ Java Basics 3/jb3Assignment3/src/com/ss/basics3/three/App.java
@@ -1,7 +1,40 @@
 package com.ss.basics3.three;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        App testRead = new App();
+        final String path = "D:\\Downloads\\Personal\\Smoothstack\\SmoothstackEssentials\\Java Basics 3\\testFiles\\jb3Assignment3TestDoc.txt";
+        char searchChar = 'e';
+        //store the value returned which will either be -1 for failing to find the file or the count of searchChar in the file
+        int appeared = testRead.readFile(path, searchChar);
+        //If failed to read file, exit
+        if(appeared == -1){
+            System.exit(0);
+        }
+        //If file read, print out the char and how many were found
+        else{
+            System.out.println("The character " + searchChar + " appeared " + appeared + " times!");
+        }
+    }
+    public Integer readFile(String path, Character c){
+        try(BufferedReader bfr = new BufferedReader(new FileReader(path))){
+            int ch = 0,counter = 0;
+            //While the incoming character from the reader (read as an int) is not -1 (would mean end of file) 
+            while((ch=bfr.read())!=-1){
+                //If the character read (casted to char from int) is the one we're looking for, add to counter
+                if((char)ch == c){
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        catch(IOException e){
+            System.err.println("Could not find file, please check path.");
+            return -1;
+        }
     }
 }
